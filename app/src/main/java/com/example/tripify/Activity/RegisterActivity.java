dd940863b5c7bc8c70dcceacb3e133e2f538467b
Al-ameen Abdulkareem <abdulkareemalameen18@gmail.com>
2023-04-30 04:12:30 +0100

    legit_commit_1

--- app/src/main/java/com/example/tripify/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/tripify/Activity/RegisterActivity.java
@@ -4,17 +4,18 @@ import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tripify.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -25,7 +26,7 @@ public class RegisterActivity extends AppCompatActivity {
     Button register_btn;
     final String email_pattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]+";
     ProgressDialog mLoadingBar;
-    ProgressBar progressBar;
+    ProgressDialog progressDialog;
     FirebaseAuth mAuth;
     TextView textView;
 
@@ -46,7 +47,7 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        progressDialog = new ProgressDialog(this);
 
         textView = findViewById(R.id.got_account_sign_in);
 
@@ -67,7 +68,7 @@ public class RegisterActivity extends AppCompatActivity {
 
 
 
-        //register_btn.setOnClickListener(view -> checkCredentials());
+        register_btn.setOnClickListener(view -> checkCredentials());
         //btn.setOnClickListener((v) -> startActivity(new Intent(RegisterActivity.this, LoginActivity.class)));
     }
 
@@ -99,50 +100,29 @@ public class RegisterActivity extends AppCompatActivity {
             mLoadingBar.setCanceledOnTouchOutside(false);
             mLoadingBar.show();
 
-            register_btn.setOnClickListener(view -> {
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(intent);
-                finish();
-            });
-
-            buttonReg.setOnClickListener(new View.OnClickListener() {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
-                public void onClick(View view) {
-                    progressBar.setVisibility(View.VISIBLE);
-                    String email, password;
-                    email = String.valueOf(editTextEmail.getText());
-                    password = String.valueOf(editTextPassword.getText());
-
-                    if (TextUtils.isEmpty(email)) {
-                        Toast.makeText(Register.this, "Enter email", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-                    if (TextUtils.isEmpty(password)) {
-                        Toast.makeText(Register.this, "Enter password", Toast.LENGTH_SHORT).show();
-                        return;
-                    }
-
-
-            mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(task -> {
-                    progressBar.setVisibility(View.GONE);
+                public void onComplete(@NonNull Task<AuthResult> task) {
                     if (task.isSuccessful()) {
-
-                        Toast.makeText(RegisterActivity.this, "Account created.",
-                                Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        startActivity(intent);
-                        finish();
+                        progressDialog.dismiss();
+                        sendUserToNextActivity();
+                        Toast.makeText(RegisterActivity.this, "Account created.", Toast.LENGTH_SHORT).show();
                     } else {
                         // If sign in fails, display a message to the user.
-                        Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                                Toast.LENGTH_SHORT).show();
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
 
                     }
-                });
+                }
+            });
+
         }
     }
+    private void sendUserToNextActivity(){
+            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        }
 }
 
 
--- app/src/main/java/com/example/tripify/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/tripify/Fragment/HomeFragment.java
@@ -1,26 +1,7 @@
 package com.example.tripify.Fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import androidx.fragment.app.Fragment;
-
-import com.example.tripify.R;
 
 public class HomeFragment extends Fragment {
 
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
 }
